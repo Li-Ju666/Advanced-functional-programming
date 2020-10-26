355044521781da7079def2abe0a859f977b18055
sariel <li.ju.0122@student.uu.se>
2020-10-27 00:04:50 +0100

    test on erlang

--- erlang/tut.erl
+++ erlang/tut.erl
@@ -1,5 +1,5 @@
 -module(tut).
--export([fac/1, double/1, mult/2, convert/1]).
+-export([fac/1, double/1, mult/2, convert/1, list_length/1]).
 
 double(X) -> 2*X.
 
@@ -10,3 +10,8 @@ mult(X,Y) -> X*Y.
 
 convert({cm, X}) -> {inch, X/2.54};
 convert({inch, X}) -> {cm, X*2.54}.
+
+list_length(Xs)->list_length(Xs,0).
+
+list_length([], Acc) -> Acc;
+list_length([_|Tail], Acc)->list_length(Tail, Acc+1).
