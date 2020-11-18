8f0fb26ec1d4fc46c358fb79354926b2d3963851
liju666 <li.ju.0122@student.uu.se>
2020-11-18 23:56:07 +0100

    p1 test

--- as2/p1/Perm.hs
+++ as2/p1/Perm.hs
@@ -1,6 +1,7 @@
 module Perm(perm) where
 
 import Test.QuickCheck
+import Data.List(sort, nub)
 
 perm:: [Int] -> [Int] -> Bool
 perm input output = permS (reverse input) (reverse output) []
@@ -23,13 +24,25 @@ permS input@(i:is) output@(o:os) stack@(s:ss)
     | s == o = permS input os ss
     | otherwise = permS is output (i:stack)
 
-prop_reverse :: [Int] -> Bool
-prop_reverse xs = reverse (reverse xs) == xs
-
 -- ==============================================================
 -- Property-based test for perm:
 
 -- property 1: for dequeuing order i j k, if i<j<k while Qk<Qi<Qj, 
 -- the input queue is not stack-sortable
-prop_unsortable :: [Int] -> [Int] -> Bool
-prop_unsortable = undefined
+prop_unsortable x = (x>3) ==> (pattern_test [x, x-1 .. 1])
+
+pattern_test :: [Int] -> Bool
+pattern_test xs = False == perm (reverse q231) xs
+    where q231 = generate231(xs)
+
+generate231 :: [Int] -> [Int]
+generate231 (x1:x2:x3:xs) = mid:max:min:xs
+    where [min, mid, max] = sort [x1, x2, x3]
+
+-- Property 2: lists with less than 1,000,000 elements can 
+-- be computed with in one second
+prop_eff x = (x>0 and x<1000000) ==> eff_test [x,x-1..0]
+    where types = x::Int
+
+-- eff_test xs = 
+
