89bb6eac33e00a62807d504636543fd3281014bc
liju666 <li.ju.0122@student.uu.se>
2020-11-22 15:05:58 +0100

    p2 algorithm implementation

--- as2/p2/Kcolor.hs
+++ as2/p2/Kcolor.hs
@@ -1,7 +1,8 @@
 module Kcolor where
 
 import Test.QuickCheck
-import Data.List(nub)
+import Data.List(nub, sort)
+
 
 type Vert = Int
 type Edge = (Vert, Vert)
@@ -48,12 +49,12 @@ getEdges :: Graph -> [Edge]
 getEdges g = getEdgesAcc g []
 
 getEdgesAcc [] acc = acc
-getEdgesAcc ((v,adj):vs) acc = getEdgesAcc vs [(v,x)|x<-adj]
+getEdgesAcc ((v,adj):vs) acc = getEdgesAcc vs [(v,x)|x<-adj]++acc
 
 -- Property-based test for graph data structure
 -- 1. pairwise adjacent vertex testing
 
-prop_pairwise_adj l = 
+prop_pairwiseAdj l = 
     forAll (randomGraph (length l) l newGraph) $ pairwiseTest
 
 randomGraph :: Int -> [Int] -> Graph -> Gen Graph
@@ -69,3 +70,46 @@ pairwiseTest g =
         (v1:_) -> case getAdj g v1 of
                     [] -> True
                     (v2:_) -> elem v1 (getAdj g v2)
+
+-- Property-based test for graph data structure
+-- 2. to check the set of vertices is the subset of 
+-- set of all vertices
+prop_edgeVert l = 
+    forAll (randomGraph (length l) l newGraph) $ edgeVertTest
+
+edgeVertTest [] = True
+edgeVertTest g = 
+    foldr (\x acc -> if elem x verts then acc else False) True edgeVerts
+    where edges = getEdges g
+          verts = nub $ getVerts g
+          edgeVerts = nub $ foldr (\(v1,v2) acc -> v1:v2:acc) [] edges
+
+-- Kcolor problem: implementation of brute-force algorithm
+type Color = (Vert, Char)
+kcolor :: Graph -> Int -> Maybe [Color]
+kcolor g num = solve g g (Just []) (getColors num)
+
+solve g [] colored _ = do
+    allColors <- colored
+    if isValid g allColors then Just (sort allColors) else Nothing
+solve g ((v,_):vs) colored colors = do
+    allColored <- colored
+    curColor <- return [(v,x):allColored|x<-colors]
+    foldl (\acc x -> 
+        if acc == Nothing then solve g vs (Just x) colors 
+                          else acc) Nothing curColor
+
+getColors :: Int -> [Char]
+getColors n = take n ['a'..'z']
+
+isValid :: Graph -> [Color] -> Bool
+isValid g [] = False
+isValid g colors = 
+    foldl checkColor True edges
+    where
+        edges = getEdges g
+        checkColor acc (v1,v2) = 
+            if acc then vertColor v1 colors /= vertColor v2 colors
+            else acc
+        vertColor v1 ((v2,c):vs) = if v1 == v2 then c else vertColor v1 vs
+
